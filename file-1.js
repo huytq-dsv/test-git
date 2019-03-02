21eb1fe0f35f11968bdd29da975e62b1b401d6cb
Andy Devstic <huytq@dgroup.co>
2019-03-02 15:03:01 +0700

    Third feature

--- file-1.js
+++ file-1.js
@@ -1,4 +1,8 @@
 console.log('this is first conent')
 console.log('Second feature - andy')
+<<<<<<< HEAD
 asdasdasd
 add some thing to feature 1
+=======
+console.log('Third feature - andy')
+>>>>>>> Third feature
